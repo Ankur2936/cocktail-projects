a490c59276bbd2e307024056ae95bd2d165e1d65
Ankur Kumar <ankur.kumar@merittrac.com>
2023-10-26 11:14:32 +0530

    chnages_in_routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { CocktailComponent } from './cocktail/cocktail.component';
 import { CocktailDetailsComponent } from './cocktail/cocktail-details/cocktail-details.component';
 
 export const APP_ROUTES: Routes = [
-  { path: '', redirectTo: '/cocktail', pathMatch: 'full' },
+  { path: '', redirectTo: 'cocktail', pathMatch: 'full' },
   { path: 'cocktail', component: HomeComponent },
   { path: 'details', component: CocktailDetailsComponent},
   { path: 'margarita', component: CocktailComponent },
